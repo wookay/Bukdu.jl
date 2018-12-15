eacf63d6758033f408393355981c59f1e9a5b0e8
WooKyoung Noh <wookay.noh@gmail.com>
2018-12-15 18:45:01 +0900

    refactor REST example

--- examples/rest/ex1.jl
+++ examples/rest/ex1.jl
@@ -7,17 +7,22 @@ struct RestController <: ApplicationController
     conn::Conn
 end
 
-function init(c::RestController)
-    region::String = c.params.region
-    site_id::Int, channel_id::Int = parse.(Int, (c.params.site_id, c.params.channel_id))
-    render(JSON, (region, site_id, channel_id))
+function init(c::RestController, region::String, site_id::Int, channel_id::Int)
+    render(JSON, (:init, region, site_id, channel_id))
 end
 
-function update(c::RestController)
+function update(c::RestController, region::String, site_id::Int, channel_id::Int)
+    render(JSON, (:update, region, site_id, channel_id))
+end
+
+function build_params(c::RestController)
     region::String = c.params.region
     site_id::Int, channel_id::Int = parse.(Int, (c.params.site_id, c.params.channel_id))
-    render(JSON, (region, site_id, channel_id))
+    (c, region, site_id, channel_id)
 end
+
+init(c::RestController) = init(build_params(c)...)
+update(c::RestController) = update(build_params(c)...)
 
 routes() do
     get("/init/region/:region/site/:site_id/channel/:channel_id/", RestController, init)
@@ -25,3 +30,8 @@ routes() do
 end
 
 Bukdu.start(8080)
+
+#=
+curl localhost:8080/init/region/west/site/1/channel/2/
+curl localhost:8080/update/region/west/site/1/channel/2/
+=#
