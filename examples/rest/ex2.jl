53ad1873bd10d9372c8a38c3e0dd294f944cee95
WooKyoung Noh <wookay.noh@gmail.com>
2019-01-08 17:44:48 +0900

    payload

--- examples/rest/ex2.jl
+++ examples/rest/ex2.jl
@@ -8,7 +8,7 @@ struct RESTController <: ApplicationController
 end
 
 function create(c::RESTController)
-    @info :message something(c.params.message, "emtpy")
+    @info :payload (c.params.message, c.params.x, c.params.y)
     render(JSON, "OK")
 end
 
@@ -20,5 +20,6 @@ end
 Bukdu.start(8080)
 
 #=
-curl -H "Content-Type: application/json" http://127.0.0.1:8080/messages -d '{"message":"Hello Data"}'
+curl -H "Content-Type: application/json" http://127.0.0.1:8080/messages -d '{"message": "Hello Data"}'
+curl -H "Content-Type: application/json" http://127.0.0.1:8080/messages -d '{"x": 1.2, "y": 2.3}'
 =#
