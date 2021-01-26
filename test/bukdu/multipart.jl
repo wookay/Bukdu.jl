0660fd304cea11ab78713416a9eb4d588fc35b06
WooKyoung Noh <wookay.noh@gmail.com>
2021-01-27 01:16:11 +0900

    `HTTP.post(url, f::Form; kw...)` is deprecated, use `post(url, [], f; kw...)` instead.

--- test/bukdu/multipart.jl
+++ test/bukdu/multipart.jl
@@ -22,11 +22,11 @@ Bukdu.start(8191, host="127.0.0.1")
 
 part1 = Multipart("mycoolfile1.txt", IOBuffer("hello"))
 part2 = Multipart("mycoolfile2.txt", IOBuffer("world"))
-r = HTTP.post("http://127.0.0.1:8191/upload", Form(["user_file1" => part1, "user_file2" => part2]))
+r = HTTP.post("http://127.0.0.1:8191/upload", [], Form(["user_file1" => part1, "user_file2" => part2]))
 @test r.body == Vector{UInt8}("""(["mycoolfile1.txt", "hello"], ["mycoolfile2.txt", "world"])""")
 
 seekstart(part2.data)
-r = HTTP.post("http://127.0.0.1:8191/upload", Form(["user_file1" => Multipart("", IOBuffer()), "user_file2" => part2]))
+r = HTTP.post("http://127.0.0.1:8191/upload", [], Form(["user_file1" => Multipart("", IOBuffer()), "user_file2" => part2]))
 @test r.body == Vector{UInt8}("""(["", ""], ["mycoolfile2.txt", "world"])""")
 
 Bukdu.stop()
