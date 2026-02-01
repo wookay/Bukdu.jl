ffcec067e31c9e2e502963a397b17a1c08148d1f
WooKyoung Noh <wookay.noh@gmail.com>
2026-02-01 14:54:05 +0900

    compat JSON 1.4 and update test json parse.jl

--- test/json/parse.jl
+++ test/json/parse.jl
@@ -6,14 +6,14 @@ using JSON
 json_decode = JSON.parse
 json_encode = JSON.json
 
-@test_throws ErrorException json_decode(IOBuffer())  #
-@test_throws ErrorException json_decode(IOBuffer("1"))  #
+@test_throws ArgumentError json_decode(IOBuffer())
+@test json_decode(IOBuffer("1")) == 1
 @test json_decode(String(take!(IOBuffer("1")))) == 1
 @test json_decode(read(IOBuffer("1"), String)) == 1
 @test (json_encode ∘ json_decode)(IOBuffer("[1]"))   == "[1]"
 @test (json_encode ∘ json_decode)(IOBuffer("[1.0]")) == "[1.0]"
 
-@test_throws ErrorException json_decode("")  #
+@test_throws ArgumentError json_decode("")
 @test json_decode("1")   isa Int
 @test json_decode("1.0") isa Float64
 @test (json_encode ∘ json_decode)("[1]")   == "[1]"
@@ -30,7 +30,7 @@ using JSON2
 json_decode = JSON2.read
 json_encode = JSON2.write
 
-@test json_decode(IOBuffer()) == NamedTuple()
+@test json_decode(IOBuffer()) == NamedTuple()  #
 @test json_decode(IOBuffer("1")) == 1
 @test json_decode(String(take!(IOBuffer("1")))) == 1
 @test json_decode(read(IOBuffer("1"), String)) == 1
@@ -54,14 +54,14 @@ using JSON3
 json_decode = JSON3.read
 json_encode = JSON3.write
 
-@test_throws ArgumentError json_decode(IOBuffer()) #
+@test_throws ArgumentError json_decode(IOBuffer())
 @test json_decode(IOBuffer("1")) == 1
 @test json_decode(String(take!(IOBuffer("1")))) == 1
 @test json_decode(read(IOBuffer("1"), String)) == 1
 @test (json_encode ∘ json_decode)(IOBuffer("[1]"))   == "[1]"
 @test (json_encode ∘ json_decode)(IOBuffer("[1.0]")) == "[1]"  #
 
-@test_throws ArgumentError json_decode("") #
+@test_throws ArgumentError json_decode("")
 @test json_decode("1")   isa Int
 @test json_decode("1.0") isa Int  #
 @test (json_encode ∘ json_decode)("[1]")   == "[1]"
